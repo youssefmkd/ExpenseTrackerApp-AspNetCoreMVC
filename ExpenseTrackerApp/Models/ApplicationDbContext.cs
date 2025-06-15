a6ccb5ecad9fd0cde618680e619647f12d06c257
Youssef MOUKADEM <moukadem.youssef.r4i@student.ucv.ro>
2025-06-15 21:41:40 +0100

    initial commit

---
+++ ExpenseTrackerApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerApp.Models
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options):base(options)
+		{}
+
+		public DbSet<transaction> Transactions { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
---
+++ ExpenseTrackerApp/Program.cs
@@ -0,0 +1,41 @@
+using ExpenseTrackerApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //DI
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2XFhhQlJHfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTH5Vd0RjUX1WcHJTRmlfWkZ/");
+
+			var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
